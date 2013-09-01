495cb25d637d5f5c71c32c59f4c2cc11de484b63
DouglasCodes <douglasking215@gmail.com>
2013-08-31 23:08:37 -0400

    Building an Option object to pass to the menu generators.

---
+++ Steakys/Options.pm
@@ -0,0 +1,31 @@
+package Steakys::Options;
+use 5.013;    # 5.014
+use strict;
+use warnings;
+use Moose;
+sub _build_expression();
+
+has 'title'         => ( is => 'rw', isa => 'Str' );
+has 'fun_direction' => ( is => 'rw', isa => 'CodeRef' );
+has 'expression'    => (
+    is      => 'rw',
+    lazy    => 1,
+    builder => '_build_expression',
+    isa     => 'Str'
+);
+
+# sub BUILD {
+# }
+
+sub _build_expression() {
+    my $self = shift;
+	$self->title() =~ s/ +/|/rg ;
+};
+
+around BUILDARGS => sub {
+    my $orig  = shift;
+    my $class = shift;
+    return $class->$orig(@_);
+};
+
+1;
--- interact.pl
+++ interact.pl
@@ -3,10 +3,12 @@ use strict;
 use warnings;
 use lib './';
 use Steakys::Schema;
+use Steakys::Options;
 sub login_screen();
 sub create_user();
 sub leave();
 sub session();
+sub help();
 sub show_menu();
 sub list_choices(\@);
 
@@ -23,20 +25,31 @@ my $schema = Steakys::Schema->connect(
 );
 
 # my $res = $schema->resultset('Customer')->find(8);
-login_screen();
-# show_menu();
+my $example = Steakys::Options->new( title => 'Login',
+									 fun_direction => \&session );
 
+# my $ex_string = "Currently seeing how these things work";
+# $ex_string =~ s/ +/|/g;
+# print $ex_string;
+
+# print $$example{title};
+print $example->expression;
+# $$example{fun_direction}->();
+
+# login_screen();
+# show_menu();
 
 sub login_screen() {
 
 	my $task_query = "Perform what Task?\nEx. create user\n";
-	my @options = ('Exit', 'Create User', 'Login', 'Show Menu', 'Display Past Orders');
-
-	list_choices(@options);
+	my @in_session_options = my @out_session_options = my @main_options = ('Exit', 'Help', 'Show Menu');
+	push(@out_session_options, ('Create User', 'Login'));
+	push(@in_session_options, ('Display Past Orders', 'Create New Order', 'Cancel Order'));
+	list_choices( @out_session_options) ;
 
 	print $task_query;
 	
-	while ( my $input = <> ) {	
+	while ( my $input = <>) {	
 		chomp $input;
 		given ( $input ) {
 		    leave() 				when /(0|quit|exit|leave|bye)/i;
@@ -44,9 +57,12 @@ sub login_screen() {
 		    session()		 		when /(2|(sign|log)[- ]?in)/i;
 		    show_menu()		 		when /(3|menu)/i;
 		    display_past_orders()	when /(4|orders|past)/i;
+		 	list_choices( @out_session_options ) ,
+		 		print $task_query 	when /(9|help)/i;
+
 		    default {
 		    	print "Perhaps I stuttered...\n";
-		    		list_choices(@options);
+		    		list_choices( @out_session_options ) ;
 		    }
 		}
 	}
@@ -70,17 +86,17 @@ sub display_past_orders(){
 }
 
 sub show_menu(){
-	print "I recommend you eat one of these.\n";
 	my $fullmenu = $schema->resultset("Steakys::Schema::Result::Item")->search;
 	while ( my $menu_item = $fullmenu->next ){
 		print ${menu_item}->item_name ."\t\t". ${menu_item}->price ."\n";
 	}
+	print "I recommend you eat one of these.\n";
 }
 
 sub list_choices(\@){
 	my $array = shift;
 	for (0..$#{$array}) {
-		print "$_.\t ${$array}[$_]\n"
+		print "$_.\t ${$array}[$_]\n";
 	}
 }
 
